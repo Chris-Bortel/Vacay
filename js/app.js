bcd11ae8409549fc487fceb4a646dba102d77fd6
Chris-Bortel <bortelcc19@gmail.com>
2020-05-14 14:41:06 -0700

    the weather function is pushing the sunny destinations to sunArray

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ var sunArray = [];
 var snowArray = [];
 //constructor function
 
-function Destination(name, path, sun = 0, snow = 0) {
+function Destination(name, path, sun) {
   this.name = name;
   this.path = path;
   this.sun = sun;
-  this.snow = snow;
+ 
 
   destArray.push(this);
 }
@@ -20,24 +20,42 @@ new Destination("Los Angeles", "../html/los-angeles.html", true);
 new Destination("Cabo San Lucas", "../html/cabo-san-lucas.html", true);
 new Destination("Granada", "../html/granada.html", true);
 new Destination("Phuket", "../html/phuket.html", true);
-new Destination("Stowe", "../html/stowe.html", "", true);
-new Destination("Telluride", "../html/telluride.html", "", true);
-new Destination("Tramsø", "../html/tramso.html", "", true);
-new Destination("Zermatt", "../html/zermatt.html", "", true);
+new Destination("Stowe", "../html/stowe.html", false);
+new Destination("Telluride", "../html/telluride.html", false);
+new Destination("Tramsø", "../html/tramso.html", false);
+new Destination("Zermatt", "../html/zermatt.html", false);
 console.log(destArray);
-console.log(destArray[4]);
+// console.log(destArray[4]);
 
 // Create function that iterates through destArray(to fin hot or cold option) and creates and pushes to a new temp array
 
 // ??? What is I just iterate the first for objects and push to sun array???
 
+ function weather(destArray) {
+  
+  for (var i = 0 ; i < destArray.length ; i++) {
+    if (destArray[i].sun === true) {
+      sunArray.push(destArray[i]);
+   console.log('Sunny')
+      
+    }
+  }
+}
+weather(destArray);
+
 // Destination.prototype.weather = function () {
-//   for (var i = 0; i < this.destArray.length; i++) {
-//     if (this.sun === true) {
-//       sun.push(sunArray);
-//     } else {
-//       snow.push(snowArray);
-//     }
+//   var sunArray = this.Destination([2]);
+//  if (this.sunArray.length === 0) {
+//   for(var i = 0 ; i < destArray.length ; i++){
+//         this.sunArray.push
+//        console.log('I am sunny');
+//       //  I need to figure out how to do this
+//        var sun = this.sun
+    
+//   //  }    else {
+//   //       this.sun === false;
+//   //      console.log('I am snowy');
+//   //       push[i](snowArray);
+//   //    }
 //   }
-// };
-// console.log(Destination.prototype.weather())
+// }
