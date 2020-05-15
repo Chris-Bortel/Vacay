d9b3ddb5146af6f3ecb493775efafd6972915b1b
Chris-Bortel <bortelcc19@gmail.com>
2020-05-14 19:17:12 -0700

    change constructor back to having snow

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 //global variables
 var destArray = [];
@@ -8,23 +8,24 @@ var randArray = [];
 
 //constructor function
 
-function Destination(name, path, sun) {
+function Destination(name, path, sun = 0, snow = 0) {
   this.name = name;
   this.path = path;
   this.sun = sun;
+  this.snow = snow;
 
   destArray.push(this);
 }
 
 // instantiate objects
-new Destination("Los Angeles", "../html/los-angeles.html", true);
-new Destination("Cabo San Lucas", "../html/cabo-san-lucas.html", true);
-new Destination("Granada", "../html/granada.html", true);
-new Destination("Phuket", "../html/phuket.html", true);
-new Destination("Stowe", "../html/stowe.html", false);
-new Destination("Telluride", "../html/telluride.html", false);
-new Destination("Tramsø", "../html/tramso.html", false);
-new Destination("Zermatt", "../html/zermatt.html", false);
+new Destination('Los Angeles', '../html/los-angeles.html', true);
+new Destination('Cabo San Lucas', '../html/cabo-san-lucas.html', true);
+new Destination('Granada', '../html/granada.html', true);
+new Destination('Phuket', '../html/phuket.html', true);
+new Destination('Stowe', '../html/stowe.html', false);
+new Destination('Telluride', '../html/telluride.html', false);
+new Destination('Tramsø', '../html/tramso.html', false);
+new Destination('Zermatt', '../html/zermatt.html', false);
 console.log(destArray);
 // console.log(destArray[4]);
 
@@ -54,7 +55,7 @@ function weather(destArray) {
   for (var i = 0; i < randArray.length; i++) {
     if (destArray[randArray[i]].sun === true) {
       sunArray.push(destArray[randArray[i]]);
-      console.log("Sunny");
+      console.log('Sunny');
     } else {
       snowArray.push(destArray[randArray[i]]);
       console.log('snowy')
